4ad11845aeece1cf75d71b3c2ef6bc25a5253c43
OlivaAdak <oliva.adak@gmail.com>
2020-09-12 13:31:57 +0530

    Source code added

---
+++ Controllers/PetHouseController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PetHouseDockerEg.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PetHouseDockerEg.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetHouseController : ControllerBase
+    {
+        public List<PetHouse> pet = new List<PetHouse>();
+        
+        public PetHouseController()
+        {
+            pet.Add(new PetHouse()
+            {
+                PetId = 1,
+                PetType = "Puppy",
+                Age = "3 months",
+                Price = 2000
+
+            }) ;
+            pet.Add(new PetHouse()
+            {
+                PetId = 2,
+                PetType = "cat",
+                Age = "2 months",
+                Price = 1000
+
+            });
+            pet.Add(new PetHouse()
+            {
+                PetId = 3,
+                PetType = "Parrot",
+                Age = "5 days",
+                Price = 900
+
+            });
+            
+        }
+        // GET: api/<PetHouseController>
+        [HttpGet]
+        public IEnumerable<PetHouse> Get()
+        {
+            return pet;
+        }
+
+        // GET api/<PetHouseController>/5
+        [HttpGet("{id}")]
+        public PetHouse Get(int id)
+        {
+            return pet.Where(x => x.PetId == id).FirstOrDefault();
+        }
+
+        // POST api/<PetHouseController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PetHouseController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PetHouseController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/PetHouse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetHouseDockerEg.Models
+{
+    public class PetHouse
+    {
+        public int PetId { get; set; }
+        public string PetType { get; set; }
+        public string Age { get; set; }
+        public int Price { get; set; }
+    }
+}
